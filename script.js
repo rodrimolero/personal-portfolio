8255515dce30e782a18ef3465880539b6525c819
Rodrigo Molero Roca <rodrigomolero@Rodrigos-MacBook-Air-4.local>
2026-03-15 13:55:45 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const darkModeButton = document.getElementById("darkModeToggle");
+
+    darkModeButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+
+});
