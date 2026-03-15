2d44c7f030a77fbdf09061268d1907f6ee9f323a
Rodrigo Molero <rodrigo.molero.roca@gmail.com>
2026-03-15 16:11:06 -0400

    fourth commit

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+const form = document.getElementById("contactForm");
+const message = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("message").value;
+
+    if(name === "" || email === "" || text === "") {
+
+        message.innerText = "Please fill out all fields.";
+        message.style.color = "red";
+
+    } else {
+
+        message.innerText = "Message sent successfully!";
+        message.style.color = "green";
+
+        form.reset();
+    }
+
+});
+
+const topBtn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 400) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
